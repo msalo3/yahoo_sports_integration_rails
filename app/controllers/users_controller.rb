4805c63f2c5c6c858857d4fc8610f5135a2a16a4
Marc Salo <marc@nurelm.com>
2018-03-25 15:46:27 -0400

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def index
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Thanks for signing up for The Weekly Puck!"
+      redirect_to @user
+    else
+      @messages = @user.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
