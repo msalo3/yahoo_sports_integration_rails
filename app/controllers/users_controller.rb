fad7fbbaa4be8350f026c12be735659a26b39590
Marc Salo <marc@nurelm.com>
2018-04-01 10:04:44 -0400

    Implemented basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = "Thanks for signing up for The Weekly Puck!"
       redirect_to @user
     else
