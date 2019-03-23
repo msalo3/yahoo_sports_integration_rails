9a2f7aeb771bc525e705463c34afeb6d11a46422
Marc Salo <marc@nurelm.com>
2019-03-23 13:13:16 -0400

    upadted ENVu

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :yahoo_auth, ENV['YAHOO_APP_ID'], ENV['YAHOO_APP_SECRET']
+  provider :yahoo_auth, ENV['YAHOO_CLIENT_ID'], ENV['YAHOO_APP_SECRET']
 end
